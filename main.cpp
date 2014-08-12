87adecf1f756eaaff18282c45edb0e4ac1417cd6
Marion Faugoin <marion.faugoin@gmail.com>
2014-08-12 18:40:31 +0200

    Added comments

--- main.cpp
+++ main.cpp
@@ -8,6 +8,14 @@
 #include <cstdlib>
 #include <string>
 
+/* 		
+		Le jeu que nous voulons réaliser consiste à retrouver un mot dont les lettres ont été mélangées. 
+		Mode 1 joueur: Le mot est pioché dans un fichier dictionnaire.
+		Mode 2 joueurs: Le premier doit donner un mot, le second doit le retrouver.
+
+		Dans les deux cas, le joueur a 5 essais pour trouver le bon mot, au bout desquels le mot est donné et la console demande au joueur s'il veut rejouer ou non.
+*/
+
 void deroulementJeu(int nb);
 
 using namespace std;
